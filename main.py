349e174778ce9d88e714cf55a8d477c8b280a02c
Dreamchaser4 <mchris8075@gmail.com>
2023-05-04 13:02:06 +0100

    Send an email with news titles and descriptions

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import requests
-
+from send_email import send_email
 
 api_key ="3080d4ade206453db6e996e571e5cd1e"
 url = "https://newsapi.org/v2/top-headlines?country=us&category=business&" \
@@ -12,6 +12,11 @@ request = requests.get(url)
 content = request.json()
 
 # Access the article titles and description
+body = ""
+
 for article in content["articles"]:
-    print(article["title"])
-    print(article["description"])
+    if article["title"] is not None:
+        body = body + article["title"] + "\n" + str(article["description"]) + 2*"\n"
+
+body = body.encode("utf-8")
+send_email(message=body)
